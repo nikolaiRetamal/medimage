9db70772b278383c717a712474f864da1eda3a1d
nikolaiRetamal <nikolai.retamal@yahoo.fr>
2014-08-12 14:35:39 +0200

    Affichage en console des metadonnées sur page medimage/import

--- src/main/java/cnam/medimage/controller/controleur/AccueilImportContr.java
+++ src/main/java/cnam/medimage/controller/controleur/AccueilImportContr.java
@@ -1,16 +1,19 @@
 package cnam.medimage.controller.controleur;
 
-import java.io.File;
+
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Iterator;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.dcm4che2.data.DicomElement;
 import org.dcm4che2.data.DicomObject;
 import org.dcm4che2.data.Tag;
 import org.dcm4che2.io.DicomInputStream;
+import org.dcm4che2.util.TagUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
@@ -26,45 +29,85 @@ public class AccueilImportContr implements Controller{
 		
 		ServletContext context = request.getSession().getServletContext();
 		String pathFile = context.getRealPath("/resources/dcmSamples/DEF_VEINEUX_107205/IM-0001-0001.dcm");
-		String pathFile2 = context.getRealPath("/resources/dcmSamples/DEF_VEINEUX_107205/IM-0001-0002.dcm");
-		DicomObject dcmObj;
 		DicomInputStream din = null;
 
-		//FileInputStream file = new FileInputStream(pathFile);
-		//System.out.println("file = " + file);
-		/*try {
-			File file2 = new File(pathFile2);
-			System.out.println("file2 = " + file2);
-		    din = new DicomInputStream(file2);
-		    System.out.println("din456 = " + din);
-		    dcmObj = din.readDicomObject();
-		    System.out.println("din = " + din);
-		    String uid = dcmObj.getString(Tag.ReferencedSOPInstanceUID);
-		    System.out.println("UID= " + uid);
+		FileInputStream file = new FileInputStream(pathFile);
+		System.out.println("file = " + file);
+		try {
+
+		    din = new DicomInputStream(file);
+		    listMetaInfo(din.readFileMetaInformation());
+		    listHeader(din.readDicomObject());
 		}
 		catch (IOException e) {
 		    e.printStackTrace();
-		    //return null;
 		}
 		finally {
 		    try {
-		    	//System.out.println("dinFirst = " + din);
 		        din.close();
-		        //System.out.println("dinLast = " + din);
 		    }
 		    catch (IOException ignore) {
 		    }
-		}*/
-		try {
-			din = new DicomInputStream(new FileInputStream(pathFile));
-			dcmObj = din.readDicomObject();
-			din.close();
-		} catch (IOException e) {
-			System.out.println(e.getClass().getName() + "/////////// " + e.getMessage());
 		}
-
 
 		return new ModelAndView("home");
 	}
+	
+	public void listMetaInfo(DicomObject object) {
+	   System.out.println("/////////////////" + 
+			   			  "META INFORMATION" + 
+			   			  "/////////////////");
+	   Iterator<DicomElement> iter = object.fileMetaInfoIterator();
+	   while(iter.hasNext()) {
+	      DicomElement element = (DicomElement) iter.next();
+	      int tag = element.tag();
+	      try {
+	         String tagName = object.nameOf(tag);
+	         String tagAddr = TagUtils.toString(tag);
+	         System.out.println("addr = " + tagAddr);
+	         String tagVR = object.vrOf(tag).toString();
+	         if (tagVR.equals("SQ")) {
+	            if (element.hasItems()) {
+	               System.out.println(tagAddr +" ["+  tagVR +"] "+ tagName);
+	               listMetaInfo(element.getDicomObject());
+	               continue;
+	            }
+	         }    
+	         String tagValue = object.getString(tag);
+	         System.out.println(tagAddr +" ["+ tagVR +"] "+ tagName +" ["+ tagValue+"]");
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	      }
+	   }  
+	}
+	
+	public void listHeader(DicomObject object) {
+		   System.out.println("/////////////////" + 
+		   			  "DATASET" + 
+		   			  "/////////////////");
+		   Iterator<DicomElement> iter = object.datasetIterator();
+		   while(iter.hasNext()) {
+		      DicomElement element = (DicomElement) iter.next();
+		      int tag = element.tag();
+		      if(tag == Tag.PixelData)
+		    	  break;
+		      try {
+		         String tagName = object.nameOf(tag);
+		         String tagAddr = TagUtils.toString(tag);
+		         String tagVR = object.vrOf(tag).toString();
+		         if (tagVR.equals("SQ")) {
+		            if (element.hasItems()) {
+		               System.out.println(tagAddr +" ["+  tagVR +"] "+ tagName);
+		               listHeader(element.getDicomObject());
+		               continue;
+		            }
+		         }       	 
+		         String tagValue = object.getString(tag);
+		         System.out.println(tagAddr +" ["+ tagVR +"] "+ tagName +" ["+ tagValue+"]");
+		      } catch (Exception e) {
+		         e.printStackTrace();
+		      }
+		   }  
+		}
 
 }
